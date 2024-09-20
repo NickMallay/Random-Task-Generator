42e4cce8046673348f5cb98d9528bcf68f68ef10
Nick Mallay <nickmallay@hotmail.com>
2024-09-20 17:13:33 -0400

    Basic task-list and print functionality

---
+++ RandomTaskGenerator.py
@@ -0,0 +1,21 @@
+## A random task generator to refresh my python skills. I am also working on my typing at the moment, so I will be focusing on proper form over speed.
+## Prioritize good practises - work iterively, comment often. 
+
+#Feature Specifications
+# User added tasks
+# Strike completed tasks
+# Generate a random Task
+# Users can give or change the weight of a task
+# Display all tasks
+
+
+tasks = [
+    "Learn Python",
+    "Practise Git",
+    "Work on a project",
+    "Take a break"
+]
+
+
+for task in tasks:
+    print(task)
